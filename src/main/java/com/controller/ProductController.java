6b442b5aa697eec4eaa53dfd6bf5026bba1f4882
Tejas Shah <tejasshah2k19@gmail.com>
2024-10-26 16:47:51 +0530

    list 1.0

--- src/main/java/com/controller/ProductController.java
+++ src/main/java/com/controller/ProductController.java
@@ -1,7 +1,10 @@
 package com.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -29,4 +32,16 @@ public class ProductController {
 		productDao.addProduct(productBean);
 		return "";
 	}
+	
+	@GetMapping("listproducts")
+	public String listProduct(Model model) {
+		
+		//get list from database 
+		List<ProductBean> productList = productDao.getAllProducts();	
+		//send list to jsp 
+		model.addAttribute("productList",productList);
+		System.out.println(productList.size());
+		return "ListProduct";
+	}
+	
 }
--- src/main/java/com/dao/ProductDao.java
+++ src/main/java/com/dao/ProductDao.java
@@ -1,6 +1,9 @@
 package com.dao;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -17,4 +20,11 @@ public class ProductDao {
 		stmt.update("insert into products (name,price,qty,discount,category) values (?,?,?,?,?)", productBean.getName(),
 				productBean.getPrice(), productBean.getQty(), productBean.getDiscount(), productBean.getCategory());
 	}
+
+	public List<ProductBean> getAllProducts() {
+		List<ProductBean> productList = stmt.query("select * from products",
+				new BeanPropertyRowMapper<>(ProductBean.class));
+		return productList; 
+	}
+
 }
